879779a33ff3a7bbefb9134fd651129fc135dab4
Sebastián <sebaavitiello@gmail.com>
2024-09-17 15:51:26 -0300

    CHANGE: getDate function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,7 @@
-import { format, subHours } from 'date-fns';
+import { format } from 'date-fns';
 
 export const getImageUrl = (posterPath, size) => {
   return `https://image.tmdb.org/t/p/w${size}${posterPath}`;
 };
 
-export const getDate = () => {
-  const dateOffset = subHours(new Date(), 3);
-  return format(dateOffset, 'dd/MM/yyyy HH:mm');
-};
+export const getDate = () => format(new Date(), 'dd/MM/yyyy HH:mm');
